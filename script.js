78facbb979e3d14ee5865d3eb3ed37fca397c884
Vlad <100511183+VladC82@users.noreply.github.com>
2023-02-18 13:40:50 +0000

    messages

--- script.js
+++ script.js
@@ -4,7 +4,12 @@ const overlay = document.getElementById("overlay");
 const cartDiv = document.getElementById("cartdiv");
 const modal = document.getElementById("modal");
 
+// cart and checkout buttons added
 const checkOutBtn = document.getElementById("checkout");
 const addToCart = document.getElementById("addtocart");
 const price = document.getElementById("price");
 const total = document.getElementById("total");
+
+const orderNumber = document.getElementsByClassName("cartnumber");
+const cartMessages = document.getElementById("cartmodal");
+const emptyCartMsg = document.getElementById("emptymessage");
