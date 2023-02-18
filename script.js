9ba0d82f140c6c22f63b696e232535a23823bb8e
Vlad <100511183+VladC82@users.noreply.github.com>
2023-02-18 13:47:08 +0000

    icons in script

--- script.js
+++ script.js
@@ -10,6 +10,14 @@ const addToCart = document.getElementById("addtocart");
 const price = document.getElementById("price");
 const total = document.getElementById("total");
 
+//messages
 const orderNumber = document.getElementsByClassName("cartnumber");
 const cartMessages = document.getElementById("cartmodal");
 const emptyCartMsg = document.getElementById("emptymessage");
+
+//icons
+const substractItem = document.getElementById("substract");
+const addItem = document.getElementById("add");
+const cartIcon = document.getElementById("cart");
+const delBtn = document.getElementById("delete");
+const cancelBtn = document.getElementById("cancel");
