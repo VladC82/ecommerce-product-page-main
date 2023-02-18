1e497d896f8e9b184946b8f17b0df319e4ff8b67
Vlad <100511183+VladC82@users.noreply.github.com>
2023-02-18 13:24:08 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const mobileMenu = document.getElementById("mobilemenu");
+const navMenu = document.getElementById("navbar");
+const overlay = document.getElementById("overlay");
+const cartDiv = document.getElementById("cartdiv");
+const modal = document.getElementById("modal");
+
+const checkOutBtn = document.getElementById("checkout");
+const addToCart = document.getElementById("addtocart");
